6f52e028cd65d65908df172bdeb9d0cec2fee2de
Gabriel Chavez DS <chavez_camargo1@hotmail.com>
2024-07-18 01:12:31 -0600

    Creando repo

---
+++ container/b01_container_streamlit.py
@@ -0,0 +1,8 @@
+import os, sys
+sys.path.append(os.getcwd())
+import streamlit as st 
+import pandas as pd 
+
+
+
+
---
+++ controller/a01_preproceso.py
@@ -0,0 +1,14 @@
+
+import os, sys
+sys.path.append(os.getcwd())
+# Lee los datos de ventas
+import pandas as pd 
+
+#df = pd.read_csv('')
+
+# Módulo para almacenar las variables calculadas
+#class DataStorage:
+ #   df_ventas = None
+
+
+    
