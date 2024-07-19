a03a4acd5ca426a1802303377893e8f02497f961
Gabriel Chavez DS <chavez_camargo1@hotmail.com>
2024-07-19 08:26:39 -0600

    agregando_df

--- controller/a01_preproceso.py
+++ controller/a01_preproceso.py
@@ -11,4 +11,22 @@ import pandas as pd
  #   df_ventas = None
 
 
-    
+df_itens_pedidos = pd.read_csv('base/data/df_itens_pedidos.csv')
+df_itens_pedidos.head()
+
+df_pedidos = pd.read_csv('base/data/df_pedidos.csv')
+df_pedidos.head()
+
+df_productos = pd.read_csv('base/data/df_productos.csv')
+df_productos.head()
+
+df_vendedores = pd.read_csv('base/data/df_vendedores.csv')
+df_vendedores.head()
+
+
+
+def preprocesamiento(df_itens_pedidos,df_pedidos,df_productos,df_vendedores):
+
+
+    return df_itens_pedidos,df_pedidos,df_productos,df_vendedores
+
